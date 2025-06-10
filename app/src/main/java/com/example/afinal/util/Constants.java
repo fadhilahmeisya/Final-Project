167158f837dc09f7896e1141d51131249a8fe158
Fadhilah Meisya Az Zahrah <143792903+fadhilahmeisya@users.noreply.github.com>
2025-06-11 06:20:13 +0800

    Revert "four"
    This reverts commit aad1b8b792bdd8626f43b6a791daeda643c4c706.

--- app/src/main/java/com/example/afinal/util/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.afinal.util;
-
-public final class Constants {
-    private Constants() {}
-
-    public static final String API_KEY = "GANTI_DENGAN_API_KEY_ANDA";
-    public static final String BASE_URL = "https://newsapi.org";
-}
